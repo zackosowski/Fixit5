e53ad3da61eddfb06cdede6c2f8b9bf3e1bfafeb
zackosowski <ZACHARYO@MYSTMA.ORG>
2020-10-27 13:47:43 +0000

    created assignment

--- JavaTemplate/src/app/App.java
+++ JavaTemplate/src/app/App.java
@@ -1,7 +1,20 @@
 package app;
 
+/*
+FIXIT 5
+
+This programming should keep running as long as the user keeps entering yes.
+*/
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        Scanner input = new Scanner(System.in);
+
+        String answer = "no";
+
+        while(answer == "yes"){
+            System.out.println("Do you want this to keep running? Type yes or no.");
+            answer = input.nextInt();
+        }
     }
 }
